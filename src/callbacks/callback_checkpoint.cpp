827b28bd20bb7cf1986d1e5604ed756cad68dc61
DylanMcKinney <mckinney@catalyst160.llnl.gov>
2017-12-12 14:15:35 -0800

    Added checkpoint to file per rank. Set option with m_checkpoint_per_rank in prototext. Many file writes are within if(rank==0) statements. For now this was tested by removing these conditionals

--- src/callbacks/callback_checkpoint.cpp
+++ src/callbacks/callback_checkpoint.cpp
@@ -161,7 +161,7 @@ bool lbann_callback_checkpoint::checkpointShared(model *m) {
   snprintf(epochdir, sizeof(epochdir), "%s/shared.epoch.%d.step.%d", dir, epoch, step);
   // start our checkpoint
   persist p;
-  p.open_checkpoint(epochdir);
+  p.open_checkpoint(epochdir,m_checkpoint_per_rank);
   // call virtual function to checkpoint model state
   m->save_to_checkpoint_shared(p);
   // close our checkpoint
@@ -222,7 +222,7 @@ bool lbann_callback_checkpoint::restartShared(model *m) {
   sprintf(epochdir, "%s/shared.epoch.%d.step.%d", dir, epoch, step);
   // open our checkpoint
   persist p;
-  p.open_restart(epochdir);
+  p.open_restart(epochdir,m_checkpoint_per_rank);
   // call virtual function to restore model from checkpoint
   m->load_from_checkpoint_shared(p);
   // close our checkpoint
@@ -243,5 +243,42 @@ bool lbann_callback_checkpoint::restartShared(model *m) {
   }
   return true;
 }
+
+bool lbann_callback_checkpoint::savePerRank(model *m){
+ /* if (m_checkpoint_dir.length() == 0) {
+    return false;
+  }
+  // time how long this takes
+  El::Timer timer;
+  lbann_comm *comm = m->get_comm();
+  // get checkpoint directory
+  const char *dir = m_checkpoint_dir.c_str();
+  // read current epoch and step counters from model
+  int epoch = m->get_cur_epoch();
+  int step  = m->get_cur_step();
+  // let user know we're saving a checkpoint
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (comm->am_world_master()) {
+    timer.Start();
+    printf("Checkpoint: epoch %d step %d ...\n", epoch, step);
+    fflush(stdout);
+  }
+  // create top level directory
+  //const char* dir = trainParams.ParameterDir.c_str();
+  makedir(dir);
+  char epochdir[1024];
+  snprintf(epochdir, sizeof(epochdir), "%s/shared.epoch.%d.step.%d", dir, epoch, step);
+  makedir(epochdir);
+  strcpy(m_checkpoint_dir, epochdir);
+  
+  */
+  return true;
+}
+
+bool lbann_callback_checkpoint::loadPerRank(model *m){
+  return true;
+}
+
+
 
 }
