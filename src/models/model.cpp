c61fff9bccb58b8ea17d1ce6ade0862d02f0128b
Jae-Seung Yeom <yeom2@llnl.gov>
2018-03-14 13:45:22 -0700

    before storing checkpoint states of a model, make sure the states are copied to host if they are on GPU after loading checkpoint states of a model, make sure the states are copied to device if they are supposed to be on GPU

--- src/models/model.cpp
+++ src/models/model.cpp
@@ -1096,6 +1096,13 @@ bool model::save_to_checkpoint_shared(persist& p, bool val_end) {
     }
     
     for (weights *w : m_weights) {
+      w->set_states_on_host();
+    }
+    if (!m_weights.empty()) {
+      m_weights.at(0)->synchronize();
+    }
+
+    for (weights *w : m_weights) {
       w->save_to_checkpoint_shared(p);
     }
     for (size_t l = 0; l < m_layers.size(); l++) {
@@ -1158,6 +1165,7 @@ bool model::load_from_checkpoint_shared(persist& p) {
   //}
   for (weights *w : m_weights) {
     w->load_from_checkpoint_shared(p);
+    w->set_states_on_device(); // only if needed
   }
   // read in each layer
   for (size_t l = 0; l < m_layers.size(); l++) {
@@ -1165,6 +1173,10 @@ bool model::load_from_checkpoint_shared(persist& p) {
       return false;
     }
   }
+  if (!m_weights.empty()) {
+    m_weights.at(0)->synchronize();
+  }
+
   return true;
 }
 
